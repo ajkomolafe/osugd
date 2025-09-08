44576906c5189042a8b32391cda1f4551cfd215a
ajkomolafe <141524434+ajkomolafe@users.noreply.github.com>
2025-09-07 23:40:53 -0500

    add login oauth base functionality

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="js">
   import { Button } from "$lib/components/ui/button/index.js";
+  import { page } from '$app/state'
+
+  const client_id = import.meta.env.VITE_CLIENT_ID
+
+  if (page.url.searchParams.get('code') != null) {
+    //set cookie, move this to +page.server.js load
+  }
 </script>
  
-<Button>Click me</Button>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button href="https://osu.ppy.sh/oauth/authorize?client_id={client_id}&response_type=code&scope=public+identify">Login With osu!</Button>
