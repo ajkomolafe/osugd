96d65f0b1ab2566588501a9734fc3239f536ac72
ajkomolafe <141524434+ajkomolafe@users.noreply.github.com>
2025-10-07 01:37:55 -0300

    finished reminder job

--- backend/src/api.js
+++ backend/src/api.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose"
 import userRouter from "./users/users.js"
 import authRouter from "./auth/auth.js"
 import Banchojs from "bancho.js"
+import Reminder from './users/reminder.js'
+import Beatmapset from './users/beatmapset.js'
 
 dotenv.config()
 const api = express();
@@ -16,11 +18,7 @@ const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Database"));
 
-const client = new Banchojs.BanchoClient({ username: process.env.IRC_USERNAME, password: process.env.IRC_PASSWORD });
-client.connect().then(async () => {
-    console.log("Connected to Bancho");
-    // await client.getUser("Mildly Accurate").sendMessage("test")
-}).catch(console.error);
+const client = new Banchojs.BanchoClient({ username: process.env.IRC_USERNAME, password: process.env.IRC_PASSWORD, apiKey: process.env.BANCHO_API_KEY });
 
 api.use(express.json());
 api.use("/api/auth", authRouter) //requests on host/auth go to authRouter
@@ -33,3 +31,88 @@ if (port == null || port == "") {
 
 api.listen(port, () => console.log("API Started"));
 
+// Every 5 minutes, check if there are reminders to be sent.
+// If a reminder should be sent, get first 3 beatmap names, and send that in the message.
+async function checkReminders() {
+	console.log("Asynchronous reminder job:\n\t" + (new Date()).toString())
+	const reminders = await Reminder.find( { } ).catch((err) => {
+		console.log(err)
+	})
+	for (const reminder of reminders){
+		if ((reminder.last_reminder + reminder.reminder_frequency) < (Date.now() / 1000)){
+			const count = await Beatmapset.countDocuments({  
+				ogd_user_id: reminder.id,
+				status: "pending",
+			});
+
+			if (count == 0){
+				// No maps to remind the user of
+				continue
+			}
+
+			const beatmapsets = await Beatmapset.find({
+				ogd_user_id: reminder.id,
+				status: "pending",
+			}).limit(3);
+
+			// Finish message setup later
+			// let message = ""
+			let message = "ogd: You have " + count + " guest difficulty to complete: " + beatmapsets[0].creator_username + "'s " + beatmapsets[0].title + ". https://ogd.akomolafe.dev"
+			// }
+
+			let user = await client.getUserById(Number(reminder.id))
+			await user.sendMessage(message)
+
+			await Beatmapset.findOneAndUpdate({
+				id: reminder.id,
+			}, {
+				last_reminder: Date.now() / 1000,
+			})
+
+			console.log("\tReminded user " + user.ircUsername)
+		}
+	}
+
+	setTimeout(checkReminders, 5 * 60 * 1000)
+}
+
+function waitForDatabase() {
+ 	if (db.readyState === 1){
+		return Promise.resolve();
+	}
+  	return new Promise((resolve, reject) => {
+		db.once("open", resolve);
+		db.once("error", reject);
+  });
+}
+
+
+function waitForBancho() {
+ 	if (client.getConnectState() === Banchojs.ConnectStates.Connected){
+		return Promise.resolve();
+	}
+  	return new Promise((resolve) => {
+		function checkConnectState() {
+			if (client.getConnectState() === Banchojs.ConnectStates.Connected){
+				resolve()
+			}
+			setTimeout(checkConnectState, 1000)
+		}
+		checkConnectState()
+  });
+}
+
+async function start() {
+  	try {
+    	client.connect()
+    	await Promise.all([waitForDatabase(), waitForBancho()]);
+		console.log("Connected to Bancho")
+
+    	checkReminders()
+  	} catch (err) {
+    	console.log(err);
+		throw err
+  }
+}
+
+start();
